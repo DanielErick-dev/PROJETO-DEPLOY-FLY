c2bfd7b4b3f39860c7d848d1d3767514c14aad24
Daniel Erick <DanielErick.estudante@gmail.com>
2023-01-04 15:16:13 -0300

    subindo alterações

--- fly_project/fly_project/asgi.py
+++ fly_project/fly_project/asgi.py
@@ -6,11 +6,8 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-import django.db.utils
 
-django.db.utils.OperationalError
 import os
-
 from django.core.asgi import get_asgi_application
 #
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fly_project.settings')
