23b875ec9176664cc78114fc0047425a6deeca14
Daniel Erick <DanielErick.estudante@gmail.com>
2023-01-04 16:18:03 -0300

    subindo alterações

---
+++ fly_project/app_fly/testes.py
@@ -0,0 +1,6 @@
+def soma(numero1, numero2):
+    return numero1 + numero2
+
+def testando_soma():
+    resultado = soma(1, 1)
+    assert resultado == 2
--- fly_project/fly_project/app_fly/tests.py
+++
@@ -1,4 +0,0 @@
-from django.test import TestCase
-
-#
-# create your tests here
